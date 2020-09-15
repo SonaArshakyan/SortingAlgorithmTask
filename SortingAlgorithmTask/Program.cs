366ea5146f79e86a7db260c9ccc8e7960d54c51b
SonaArshakyan <sona.arshakyan.00@mail.ru>
2020-09-15 15:11:50 +0500

    Sorting Taks

---
+++ SortingAlgorithmTask/Program.cs
@@ -0,0 +1,113 @@
+﻿using SortingAlgorithmsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithmTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the Size of Array");
+                int ArraySize = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Select The Algorithm you want to Perform");
+                Console.WriteLine("1.QuickSort \t2.BubbleSort  \t3.MergeSort \t4.InsertionSort \t5.CountSort");
+                int specificNumberOfAlgorithm = Convert.ToInt32(Console.ReadLine());
+                switch (specificNumberOfAlgorithm)
+                {
+                    case 1:
+                        PerformQuickSort(ArraySize);
+                        break;
+                    case 2:
+                        PerformBubbleSort(ArraySize);
+                        break;
+                    case 3:
+                        PerformMergeSort(ArraySize);
+                        break;
+                    case 4:
+                        PerformInsertionSort(ArraySize);
+                        break;
+                    case 5:
+                        PerformCountSort(ArraySize);
+                        break;
+                }
+            }
+        }
+        static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]); Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+        static void PerformQuickSort(int sizeArray)
+        {
+            SortingAlgorithms sorting = new QuickSort(sizeArray);
+            sorting.FillArray();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            sorting.SortArray();
+            watch.Stop();
+            var arr = sorting.GetSortedArray;
+            Print(arr);
+            Console.WriteLine($"Total Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+        static void PerformBubbleSort(int sizeArray)
+        {
+            SortingAlgorithms sorting = new BubbleSort(sizeArray);
+            sorting.FillArray();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            sorting.SortArray();
+            watch.Stop();
+            var arr = sorting.GetSortedArray;
+            Print(arr);
+            Console.WriteLine($"Total Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+        static void PerformMergeSort(int sizeArray)
+        {
+            SortingAlgorithms sorting = new MergeSort(sizeArray);
+            sorting.FillArray();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            sorting.SortArray();
+            watch.Stop();
+            var arr = sorting.GetSortedArray;
+            Print(arr);
+            Console.WriteLine($"Total Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+
+        static void PerformCountSort(int sizeArray)
+        {
+            SortingAlgorithms sorting = new InsertSort(sizeArray);
+            sorting.FillArray();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            sorting.SortArray();
+            watch.Stop();
+            var arr = sorting.GetSortedArray;
+            Print(arr);
+            Console.WriteLine($"Total Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+
+        static void PerformInsertionSort(int sizeArray)
+        {
+            SortingAlgorithms sorting = new CountSort(sizeArray);
+            sorting.FillArray();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            sorting.SortArray();
+            watch.Stop();
+            var arr = sorting.GetSortedArray;
+            Print(arr);
+            Console.WriteLine($"Total Execution Time: {watch.ElapsedMilliseconds} ms");
+        }        
+    }
+}
---
+++ SortingAlgorithmsLibrary/SortingAlgorithms.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithmsLibrary
+{
+    abstract public class SortingAlgorithms
+    {
+        protected int[] arr;
+        protected int sizeArray;
+
+        public SortingAlgorithms(int size)
+        {
+            sizeArray = size;
+            arr = new int[sizeArray];
+        }
+        public void FillArray()
+        {
+            int Min = 10;
+            int Max = 100;
+            Random randNum = new Random();
+            for (int i = 0; i < sizeArray; i++)
+            {
+                arr[i] = randNum.Next(Min, Max);
+            }
+        }
+        public int[] GetSortedArray { get { return arr; } }
+        public abstract void SortArray();
+        public virtual void Exchange(int[] arr, int i, int j)
+        {
+            int temp;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    public class QuickSort : SortingAlgorithms
+    {
+        public QuickSort(int size) : base(size)
+        {
+        }
+        public override void SortArray()
+        {
+            int left = 0;
+            int right = base.arr.Length - 1;
+            QucikSortArray(base.arr, left, right);
+        }
+        private void QucikSortArray(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                int pivot = Partition(arr, l, r);
+                QucikSortArray(arr, l, pivot);
+                QucikSortArray(arr, pivot + 1, r);
+            }
+        }
+        private int Partition(int[] arr, int l, int r)
+        {
+            int Pivot = arr[l];
+            int i = l; int j = r;
+            while (true)
+            {
+                while (arr[i] < Pivot)
+                    i++;
+                while (arr[j] > Pivot)
+                    j--;
+                if (i < j)
+                {
+                    Exchange(arr, i, j);
+                    i++;
+                    j--;
+                }
+                else
+                {
+                    return j;
+                }
+            }
+        }
+    }
+    public class BubbleSort : SortingAlgorithms
+    {
+        public BubbleSort(int size) : base(size) { }
+        public override void SortArray()
+        {
+            BubleSortArray(base.arr);
+        }
+        private void BubleSortArray(int[] arr)
+        {
+            int n = base.sizeArray;
+            for (int i = n - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i] < arr[j]) Exchange(arr, i, j);
+                }
+            }
+        }
+    }
+    public class MergeSort : SortingAlgorithms
+    {
+        public MergeSort(int size) : base(size) { }
+
+        public override void SortArray()
+        {
+            int left = 0;
+            int right = base.arr.Length - 1;
+            MergeSortArray(base.arr, left, right);
+        }
+        private void MergeSortArray(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (right + left) / 2;
+                MergeSortArray(arr, left, middle);
+                MergeSortArray(arr, middle + 1, right);
+                Merge(arr, left, middle, right);
+            }
+        }
+        private void Merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+            for (int ii = 0; ii < n1; ii++)
+                L[ii] = arr[l + ii];
+            for (int jj = 0; jj < n2; jj++)
+                R[jj] = arr[m + 1 + jj];
+            int i = 0;
+            int j = 0;
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+    }
+    public class CountSort : SortingAlgorithms
+    {
+        public CountSort(int size) : base(size) { }
+
+        public override void SortArray()
+        {
+            CountSortArray(base.arr);
+        }
+        private void CountSortArray(int[] arr)
+        {
+            int size = base.sizeArray;
+            int maxValueOfArray = arr.Max();
+            int[] Count = new int[maxValueOfArray + 1];
+            int[] sortedArr = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Count[arr[i]]++;
+            }
+            for (int i = 1; i <= maxValueOfArray; i++)
+            {
+                Count[i] = Count[i] + Count[i - 1];
+            }
+            for (int i = size - 1; i >= 0; i--)
+            {
+                sortedArr[--Count[arr[i]]] = arr[i];
+            }
+            base.arr = sortedArr;
+        }
+    }
+    public class InsertSort : SortingAlgorithms
+    {
+        public InsertSort(int size) : base(size)
+        {
+        }
+        public override void SortArray()
+        {
+            InsertSortArray(base.arr);
+        }
+        private void InsertSortArray(int[] arr)
+        {
+            int n = base.sizeArray;
+            for (int i = 1; i < n; i++)
+            {
+                int temp = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > temp)
+                {
+                    arr[j + 1] = arr[j]; j--;
+                }
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+
+
+}
